3331d3c671fa52df2c9c14f817ee41d5efe5af33
amy li <amy.gb.li@gmail.com>
2024-08-30 20:23:41 -1000

    Update data so markers are on the right island.

--- map.py
+++ map.py
@@ -37,8 +37,8 @@ def traps():
     locations = [{
       "name": "k56",
       "coordinates": {
-        "latitude": 21.2998,
-        "longitude": -157.8148,
+        "latitude": 22.0964,
+        "longitude": -159.5261,
       },
       "count": 0,
     }]
@@ -55,8 +55,8 @@ def traps():
     locations = [{
       "name": "k58",
       "coordinates": {
-        "latitude": 21.2998,
-        "longitude": -157.8148,
+        "latitude": 20.7984,
+        "longitude": -156.3319,
       },
       "count": 2,
     }]
@@ -64,8 +64,8 @@ def traps():
     locations = [{
       "name": "k59",
       "coordinates": {
-        "latitude": 21.2998,
-        "longitude": -157.8148,
+        "latitude": 19.5429,
+        "longitude": -155.6659,
       },
       "count": 3,
     }]
@@ -73,8 +73,8 @@ def traps():
     locations = [{
       "name": "k52", 
       "coordinates": {
-        "latitude": 21.2998,
-        "longitude": -157.8148,
+        "latitude": 21.1444,
+        "longitude": -157.0226,
       },
       "count": 4,
     }]
